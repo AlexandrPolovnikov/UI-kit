044992d05d499674a22f224040e745963d8f2176
Alexandr <91471898+AlexandrPolovnikov@users.noreply.github.com>
2023-10-14 19:45:27 +0300

    fix

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,24 @@
+import PlainButton from "./buttons/plain-button/index";
+import TagButton from "./buttons/tag-button/index";
+import ToggleButton from "./buttons/toggle-button/index";
+import CheckboxInput from "./inputs/checkbox-input/index";
+import NumberInput from "./inputs/number-input/index";
+import PasswordInput from "./inputs/password-input/index";
+import PhoneInput from "./inputs/phone-input/index";
+import RadioInput from "./inputs/radio-input/index";
+import SearchInput from "./inputs/search-input/index";
+export {
+  PlainButton,
+  TagButton,
+  ToggleButton,
+  CheckboxInput,
+  NumberInput,
+  PasswordInput,
+  PhoneInput,
+  RadioInput,
+  SearchInput,
+};
+
 export * from "./Button";
 export * from "./Accordion";
 export * from "./Icons";
